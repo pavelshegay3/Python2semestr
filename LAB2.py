0c8310f093d39f7ca5294c74998306c283f87574
pavelshegay3 <pavel1155.shegay@gmail.com>
2023-01-28 14:44:22 +0600

    Lab 2 finished

--- LAB2.py
+++ LAB2.py
@@ -1,2 +1,143 @@
-x = "Hello world"
-print(x)
+# Python Lists
+# ex1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+# ex2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+# ex3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+# ex4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+# ex5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+# ex6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+# ex7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+# ex8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+# Python Tuples
+# ex1
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+# ex2
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+# ex3
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+# ex4
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
+# Python Sets
+# ex1
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+# ex2
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+# ex3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+# ex4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+# ex5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+# Python Dictionaries
+# ex1
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car.get("model"))
+# ex2
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["year"] = 2020
+# ex3
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["color"] = "red"
+# ex4
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.pop("model")
+# ex5
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.clear()
+
+# Pythom While Loops
+# ex1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+# ex2
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+# ex3
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+# ex4
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
+# Python For Loops
+# ex1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+# ex2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+# ex3
+for x in range(6):
+    print(x)
+# ex4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 x = "Hello world"
 y = len(x)
 print(y)
-#change test number one
+# change test number one
 
-x="I love brawl stars"
-#change test number two
+x = "I love brawl stars"
+# change test number two
+
+# change test number three
+"""lists
+list items are ordered, changeable, and allow duplicate values
+list items are indexed like in c
+ordered means that the items have a defined order and that order will not change
+if you add new items to a list the new items will be placed at the end of the list
+"""
+# create a list
+thislist = ["apple", "banana", "cherry", "apple"]
+print(thislist)
+# list length
+thislist = ["apple", "apple", "cherry", "banana"]
+print(len(thislist))
+# list items-data types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9]
+list3 = [True, False, False]
+# a list can contain different data types
+list1 = ["abc", 34, True, 40, "male"]
+# type of a list
+mylist = ["water", "food"]
+print(type(mylist))
+# access items
+print(thislist[1])
+print(thislist[-1])
+print(thislist[1:4])
+print(thislist[:4])
+print(thislist[1:])
+print(thislist[-3:-1])
+# check if a specified item is present in a list
+if "apple" in thislist:
+    print("apple is in the list")
+# change item value
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi"]
