6a4d69f1c95fab8e8b8499ecfed1f80f32957e8d
pavelshegay3 <pavel1155.shegay@gmail.com>
2023-04-03 19:07:04 +0600

    Lab 8 and 9 _1 added

--- Lab8_3.py
+++ Lab8_3.py
@@ -1,57 +1,126 @@
-import pygame
-import random
-screen = pygame.display.set_mode((900, 700))
-pygame.display.set_caption('GFG Paint')
+import pygame, math
 
+pygame.init()
+
+screen = pygame.display.set_mode((900, 700))
 draw_on = False
 last_pos = (0, 0)
 
-radius = 10
+radius = 5
+font_style = pygame.font.SysFont(None, 30)
 
-def roundline(canvas, color, start, end, radius=1) :
+
+def roundline(canvas, color, start, end, radius=1):
     Xaxis = end[0] - start[0]
     Yaxis = end[1] - start[1]
     dist = max(abs(Xaxis), abs(Yaxis))
-    for i in range(dist) :
+    for i in range(dist):
         x = int(start[0] + float(i) / dist * Xaxis)
         y = int(start[1] + float(i) / dist * Yaxis)
         pygame.draw.circle(canvas, color, (x, y), radius)
-def rec(canvas, color, start, end, radius=1) :
-    Xaxis = end[0] - start[0]
-    Yaxis = end[1] - start[1]
-    dist = max(abs(Xaxis), abs(Yaxis))
-    for i in range(dist) :
-        x = int(start[0] + float(i) / dist * Xaxis)
-        y = int(start[1] + float(i) / dist * Yaxis)
-        pygame.draw.rect(canvas, color, pygame.Rect, )
 
-try :
-    while True :
+
+try:
+    blue = (0, 0, 255)
+    red = (255, 0, 0)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    yellow = (255, 255, 102)
+    green = (0, 255, 0)
+    color = black
+    screen.fill(white)
+    shape = 1
+    while True:
         e = pygame.event.wait()
-
-        if e.type == pygame.QUIT :
+        if e.type == pygame.QUIT:
             raise StopIteration
-
-        if e.type == pygame.MOUSEBUTTONDOWN :
-            # Selecting random Color Code
-            color = (random.randrange(256), random.randrange(
-                256), random.randrange(256))
-            # Draw a single circle wheneven mouse is clicked down.
+        if e.type == pygame.KEYDOWN:
+            # color selection
+            if e.key == pygame.K_r:
+                color = red
+            elif e.key == pygame.K_g:
+                color = green
+            elif e.key == pygame.K_b:
+                color = blue
+            elif e.key == pygame.K_y:
+                color = yellow
+            elif e.key == pygame.K_n:
+                color = black
+            # change radius
+            elif e.key == pygame.K_1:
+                radius = 5
+            elif e.key == pygame.K_2:
+                radius = 7
+            elif e.key == pygame.K_3:
+                radius = 9
+            elif e.key == pygame.K_4:
+                radius = 11
+            elif e.key == pygame.K_5:
+                radius = 13
+            elif e.key == pygame.K_6:
+                radius = 15
+            elif e.key == pygame.K_7:
+                radius = 17
+            elif e.key == pygame.K_8:
+                radius = 19
+            elif e.key == pygame.K_9:
+                radius = 21
+            elif e.key == pygame.K_0:
+                radius = 2
+            # eraser
+            elif e.key == pygame.K_z:
+                color = white
+            # change chape
+            elif e.key == pygame.K_x:
+                shape = 2
+            elif e.key == pygame.K_SPACE:
+                shape = 1
+            elif e.key == pygame.K_c:
+                shape = 3
+            elif e.key == pygame.K_v:
+                shape = 4
+            elif e.key == pygame.K_a:
+                shape = 5
+            elif e.key == pygame.K_s:
+                shape = 6
+        if e.type == pygame.MOUSEBUTTONDOWN and shape == 1:
             pygame.draw.circle(screen, color, e.pos, radius)
             draw_on = True
-        # When mouse button released it will stop drawing
-        if e.type == pygame.MOUSEBUTTONUP :
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            p = e.pos
+            if shape == 2:
+                pygame.draw.rect(screen, color, pygame.Rect(e.pos, (radius * 3 + (radius * 3 * 1.35), radius * 3)))
+            elif shape == 3:
+                pygame.draw.circle(screen, color, e.pos, radius)
+            elif shape == 4:
+                pygame.draw.polygon(screen, color, [p, (p[0], p[1] + radius * 3), (p[0] + radius * 3, p[1])])
+            elif shape == 5:
+                pygame.draw.polygon(screen, color, [p, (p[0] + radius * 3, p[1]),
+                                                    (p[0] + radius * 3 / 2, p[1] - (radius * 3 * math.sqrt(3) / 2))])
+            elif shape == 6:
+                pygame.draw.polygon(screen, color,
+                                    [p, (p[0] + radius * 2, p[1] + radius * 2), (p[0], p[1] + radius * 2 * 2),
+                                     (p[0] - radius * 2, p[1] + radius * 2)])
+        if e.type == pygame.MOUSEBUTTONUP:
             draw_on = False
-        # It will draw a continuous circle with the help of roundline function.
-        if e.type == pygame.MOUSEMOTION :
-            if draw_on :
+        if e.type == pygame.MOUSEMOTION:
+            if draw_on and shape == 1:
                 pygame.draw.circle(screen, color, e.pos, radius)
                 roundline(screen, color, e.pos, last_pos, radius)
             last_pos = e.pos
+        # instructions
+        i1 = font_style.render("0-9-Choose the size", True, black)
+        i2 = font_style.render("R,G,B,N,Y-Red,Green,Blue,Black,Yellow", True, black)
+        i3 = font_style.render("Z-Eraser", True, black)
+        i4 = font_style.render("SPACE-Return to the brush mode", True, black)
+        i5 = font_style.render("X-Square,C-Circle,V-Triangle#1,A-Triangle#2,S-Rhombus", True, black)
+        screen.blit(i1, (0, 0))
+        screen.blit(i2, (0, 30))
+        screen.blit(i3, (0, 60))
+        screen.blit(i4, (0, 90))
+        screen.blit(i5, (0, 120))
         pygame.display.flip()
 
-except StopIteration :
+except StopIteration:
     pass
-
-# Quit
 pygame.quit()
--- main.py
+++ main.py
@@ -1,41 +1,126 @@
-import pygame
-import random
+import pygame, math
 
 pygame.init()
 
-# Set up the display
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((900, 700))
+draw_on = False
+last_pos = (0, 0)
 
-# Set up the square
-square_size = 50
-square_x = random.randint(0, screen_width - square_size)
-square_y = random.randint(0, screen_height - square_size)
-square_color = (255, 0, 0)  # Red
+radius = 5
+font_style = pygame.font.SysFont(None, 30)
 
-# Set up the timer
-timer = pygame.time.get_ticks()
-timer_interval = 3000  # 3 seconds in milliseconds
 
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+def roundline(canvas, color, start, end, radius=1):
+    Xaxis = end[0] - start[0]
+    Yaxis = end[1] - start[1]
+    dist = max(abs(Xaxis), abs(Yaxis))
+    for i in range(dist):
+        x = int(start[0] + float(i) / dist * Xaxis)
+        y = int(start[1] + float(i) / dist * Yaxis)
+        pygame.draw.circle(canvas, color, (x, y), radius)
 
-    # Draw the square
-    screen.fill((255, 255, 255))  # White background
-    pygame.draw.rect(screen, square_color, (square_x, square_y, square_size, square_size))
 
-    # Check if the timer has expired
-    current_time = pygame.time.get_ticks()
-    if current_time - timer >= timer_interval:
-        # Change the coordinates of the square
-        square_x = random.randint(0, screen_width - square_size)
-        square_y = random.randint(0, screen_height - square_size)
-        # Reset the timer
-        timer = pygame.time.get_ticks()
+try:
+    blue = (0, 0, 255)
+    red = (255, 0, 0)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    yellow = (255, 255, 102)
+    green = (0, 255, 0)
+    color = black
+    screen.fill(white)
+    shape = 1
+    while True:
+        e = pygame.event.wait()
+        if e.type == pygame.QUIT:
+            raise StopIteration
+        if e.type == pygame.KEYDOWN:
+            # color selection
+            if e.key == pygame.K_r:
+                color = red
+            elif e.key == pygame.K_g:
+                color = green
+            elif e.key == pygame.K_b:
+                color = blue
+            elif e.key == pygame.K_y:
+                color = yellow
+            elif e.key == pygame.K_n:
+                color = black
+            # change radius
+            elif e.key == pygame.K_1:
+                radius = 5
+            elif e.key == pygame.K_2:
+                radius = 7
+            elif e.key == pygame.K_3:
+                radius = 9
+            elif e.key == pygame.K_4:
+                radius = 11
+            elif e.key == pygame.K_5:
+                radius = 13
+            elif e.key == pygame.K_6:
+                radius = 15
+            elif e.key == pygame.K_7:
+                radius = 17
+            elif e.key == pygame.K_8:
+                radius = 19
+            elif e.key == pygame.K_9:
+                radius = 21
+            elif e.key == pygame.K_0:
+                radius = 2
+            # eraser
+            elif e.key == pygame.K_z:
+                color = white
+            # change chape
+            elif e.key == pygame.K_x:
+                shape = 2
+            elif e.key == pygame.K_SPACE:
+                shape = 1
+            elif e.key == pygame.K_c:
+                shape = 3
+            elif e.key == pygame.K_v:
+                shape = 4
+            elif e.key == pygame.K_a:
+                shape = 5
+            elif e.key == pygame.K_s:
+                shape = 6
+        if e.type == pygame.MOUSEBUTTONDOWN and shape == 1:
+            pygame.draw.circle(screen, color, e.pos, radius)
+            draw_on = True
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            p = e.pos
+            if shape == 2:
+                pygame.draw.rect(screen, color, pygame.Rect(e.pos, (radius * 3 + (radius * 3 * 1.35), radius * 3)))
+            elif shape == 3:
+                pygame.draw.circle(screen, color, e.pos, radius)
+            elif shape == 4:
+                pygame.draw.polygon(screen, color, [p, (p[0], p[1] + radius * 3), (p[0] + radius * 3, p[1])])
+            elif shape == 5:
+                pygame.draw.polygon(screen, color, [p, (p[0] + radius * 3, p[1]),
+                                                    (p[0] + radius * 3 / 2, p[1] - (radius * 3 * math.sqrt(3) / 2))])
+            elif shape == 6:
+                pygame.draw.polygon(screen, color,
+                                    [p, (p[0] + radius * 2, p[1] + radius * 2), (p[0], p[1] + radius * 2 * 2),
+                                     (p[0] - radius * 2, p[1] + radius * 2)])
+        if e.type == pygame.MOUSEBUTTONUP:
+            draw_on = False
+        if e.type == pygame.MOUSEMOTION:
+            if draw_on and shape == 1:
+                pygame.draw.circle(screen, color, e.pos, radius)
+                roundline(screen, color, e.pos, last_pos, radius)
+            last_pos = e.pos
+        # instructions
+        i1 = font_style.render("0-9-Choose the size", True, black)
+        i2 = font_style.render("R,G,B,N,Y-Red,Green,Blue,Black,Yellow", True, black)
+        i3 = font_style.render("Z-Eraser", True, black)
+        i4 = font_style.render("SPACE-Return to the brush mode", True, black)
+        i5 = font_style.render("X-Square,C-Circle,V-Triangle#1,A-Triangle#2,S-Rhombus", True, black)
+        screen.blit(i1, (0, 0))
+        screen.blit(i2, (0, 30))
+        screen.blit(i3, (0, 60))
+        screen.blit(i4, (0, 90))
+        screen.blit(i5, (0, 120))
+        pygame.display.flip()
 
-    pygame.display.update()
+except StopIteration:
+    pass
+pygame.quit()
